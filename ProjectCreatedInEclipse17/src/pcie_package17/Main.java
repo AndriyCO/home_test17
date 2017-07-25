f14845500a297fee824e33d068f100ae46776ce7
AndriyCO <Aolijnyk73@gmail.com>
2017-07-25 11:58:46 +0300

    wrote change for branch master

--- ProjectCreatedInEclipse17/src/pcie_package17/Main.java
+++ ProjectCreatedInEclipse17/src/pcie_package17/Main.java
@@ -3,7 +3,7 @@ package pcie_package17;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("cange in projekt on branch master");
 
 	}
 
